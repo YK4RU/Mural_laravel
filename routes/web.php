c8b303d1e0600fe80b08d10ebcc41dc18c863b94
YK4RU <ana.lima1801@aluno.ce.gov.br>
2025-10-24 10:31:43 -0300

    feat: fazendo as rotas dos CRUDS

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,54 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//Posts CRUD
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+//Pastas CRUD
+
+Route::get('/pastas', [PastaController::class, 'index'])->name('pastas.index');
+
+Route::get('/pastas/create', [PastaController::class, 'create'])->name('pastas.create');
+Route::post('/pastas', [PastaController::class, 'store'])->name('pastas.store');
+Route::get('/pastas/{id}/edit', [PastaController::class, 'edit'])->name('pastas.edit');
+Route::put('/pastas/{id}', [PastaController::class, 'update'])->name('pastas.update');
+Route::delete('/pasta/{id}', [PastaController::class, 'destroy'])->name('pastas.destroy');
+
+
+//Comentário CRUD
+
+Route::get('/comments', [CommentController::class, 'index'])->name('comments.index');
+
+Route::get('/comments/create', [CommentController::class, 'create'])->name('comments.create');
+Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::get('/comments/{id}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+//Comentário do comentário CRUD
+
+Route::get('/commentsOfcomments', [CommentOfcommentController::class, 'index'])->name('commentsOfcomments.index');
+
+Route::get('/commentsOfcomments/create', [CommentOfcommentController::class, 'create'])->name('commentsOfcomments.create');
+Route::post('/commentsOfcomments', [CommentOfcommentController::class, 'store'])->name('commentsOfcomments.store');
+Route::get('/commentsOfcomments/{id}/edit', [CommentOfcommentController::class, 'edit'])->name('commentsOfcomments.edit');
+Route::put('/commentsOfcomments/{id}', [CommentOfcommentController::class, 'update'])->name('commentsOfcomments.update');
+Route::delete('/commentsOfcomments/{id}', [CommentOfcommentController::class, 'destroy'])->name('commentsOfcomments.destroy');
+
+//Chat CRUD
+
+Route::get('/chats', [ChatController::class, 'index'])->name('chats.index');
+
+Route::get('/chats/create', [ChatController::class, 'create'])->name('chats.create');
+Route::post('/chats', [ChatController::class, 'store'])->name('chats.store');
+Route::get('/chats/{id}/edit', [ChatController::class, 'edit'])->name('chats.edit');
+Route::put('/chats/{id}', [ChatController::class, 'update'])->name('chats.update');
+Route::delete('/chats/{id}', [ChatController::class, 'destroy'])->name('chats.destroy');
