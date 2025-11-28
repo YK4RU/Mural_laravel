3590e59597c7c17dbbdfc6490efb41dd3ab23ae5
YK4RU <ana.lima1801@aluno.ce.gov.br>
2025-11-28 16:55:10 -0300

    feat: add delete e update

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,26 @@
 <ul>
     @foreach ($posts as $post)
         <li>
-            <div>
-                <img src="{{ url('storage/' . $post->url_da_imagem) }}" alt="socorro">
-            </div>
-            <div>
-                {{ $post->descricao }}
+            <div class="border-4 rounded-md border-red-700"> 
+                <div><a href="{{ route('posts.edit', $post->id) }}">Edit</a></div>
+                <div>
+                    <form action="{{ route('posts.destroy', $post->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </div>
+                <div>
+                    <img src="{{ url('storage/' . $post->url_da_imagem) }}" alt="socorro" width="300" height="400" class="rounded-md">
+                </div>
+                <div>
+                    <a href="#">
+                        <img src="{{ asset('build/assets/imagens/comentario.png') }}" alt="perfil" class="w-10 h-10">
+                    </a>
+                </div>
+                <div class="object-top-right">
+                    {{ $post->descricao }}
+                </div>
             </div>
         </li>
     @endforeach
